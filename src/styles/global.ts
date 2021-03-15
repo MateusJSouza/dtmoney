66cf8879fe61adf102e5fe0fed3718c36505e3b7
MateusJesus <mateusjdev@gmail.com>
2021-03-15 18:25:30 -0300

    :sparkles: add CSS

---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components';
+
+// `` -> template literals
+// 
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-titles: #363F5F;
+    --text-body: #969CB3;
+
+    --green: #33CC95;
+    --shape: #FFFFFF;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-size: 16px (Desktop)
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px 
+    }
+  }
+
+  // REM = 1rem = 16px
+
+  body {
+    background: var(----background);
+    -webkit-font-smoothing: antialised;
+  }
+
+  button { 
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
